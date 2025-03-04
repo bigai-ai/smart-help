eb7e9895392a52635fc47dc36298df7353ff69f3
caozh20 <84441701+caozh20@users.noreply.github.com>
2025-03-04 14:00:12 +0800

    delete useless code

--- env/task.py
+++ env/task.py
@@ -2,19 +2,7 @@ import random
 import json
 
 def sample_task(task=None):
-    # 分几类sample
-    # Micro wave
-    # 单个task已经足够复杂了
     seed = random.random()
-
-    # return ["ArrangeRoom"]
-
-    # for data gen
-
-    # if seed < 0.5:
-    #     return ["MakeBreakfast"]
-    # else:
-    #     return ["MakeCoffee"]
 
     if task is not None:
         return [task]
@@ -26,20 +14,9 @@ def sample_task(task=None):
     else:
         return ["MakeCoffee"]
     
-# PICKUP_ABLE_OBJECT = ["Apple", "Bowl", "Bread", "ButterKnife",
-#                        "Cup", "DishSponge", "Egg", "Fork", "Knife", "Lettuce", "Mug",
-#                          "Pan", "PepperShaker", "Plate", "Pot", "Potato", "SaltShaker",
-#                            "SoapBottle", "Spatula", "Spoon", "Tomato"]
-
 PICKUP_ABLE_OBJECT = ["Apple", "Bowl", "Bread", "ButterKnife",
                        "Cup", "DishSponge", "Egg", "Fork", "Knife", "Lettuce", "Plate", "Potato"
                        , "Spatula", "Spoon", "Tomato"]
-
-# PUT_ABLE_OBJECT = [
-#     "Bowl", "Cabinet", "CounterTop", 
-#       "Fridge", "GarbageCan", "Pan", "Plate",
-#         "Pot", "Sink", "StoveBurner", "Toaster"
-# ]
     
 PUT_ABLE_OBJECT = [
       "Fridge", "Pan", "Plate",
